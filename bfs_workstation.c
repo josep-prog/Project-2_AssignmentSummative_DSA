fcaa70ae0be9d541b2b036257042c0307dd25c12
Joseph Nishimwe <j.nishimwe@alustudent.com>
2026-06-11 22:05:25 +0200

    bfs_workstation.c

--- bfs_workstation.c
+++ bfs_workstation.c
@@ -5,13 +5,13 @@
 
 // Graph as adjacency matrix with costs
 int graph[MAX][MAX] = {
-    {0, 11, 0, 0, 0, 0, 22},
-    {11, 0, 17, 0, 0, 0, 0},
-    {0, 17, 0, 22, 0, 10, 0},
-    {0, 0, 22, 0, 25, 0, 0},
-    {0, 0, 0, 25, 0, 6, 0},
-    {0, 0, 10, 0, 6, 0, 5},
-    {22, 0, 0, 0, 0, 5, 0},
+    {0, 6, 0, 0, 0, 0, 0},
+    {6, 0, 11, 5, 0, 0, 0},
+    {0, 11, 0, 17, 0, 0, 25},
+    {0, 5, 17, 0, 22, 22, 0},
+    {0, 0, 0, 22, 0, 10, 0},
+    {0, 0, 0, 22, 10, 0, 22},
+    {0, 0, 25, 0, 0, 22, 0},
 };
 
 char hubs[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G'};
@@ -35,6 +35,7 @@ int main() {
     
     int maxNeighbor = -1;
     int maxTime = -1;
+    int neighborCount = 0;
 
     // BFS setup: queue + visited set
     int queue[MAX];
@@ -52,6 +53,7 @@ int main() {
             queue[rear++] = i;
 
             printf("%c (time: %d minutes)\n", hubs[i], graph[u][i]);
+            neighborCount++;
 
             // Check if this is the highest time
             if (graph[u][i] > maxTime) {
@@ -61,9 +63,27 @@ int main() {
         }
     }
     
+    printf("\nTotal directly connected workstations found: %d\n", neighborCount);
+
     if (maxNeighbor != -1) {
-        printf("\nHighest risk exposure: %c with data transfer time %d minutes\n", 
+        printf("\nHighest risk exposure: %c with data transfer time %d minutes\n",
                hubs[maxNeighbor], maxTime);
+
+        // Check for other neighbors tied for the highest transfer time
+        int tieFound = 0;
+        for (int i = 0; i < MAX; i++) {
+            if (visited[i] && i != start && i != maxNeighbor && graph[u][i] == maxTime) {
+                if (!tieFound) {
+                    printf("Tied with: %c", hubs[i]);
+                    tieFound = 1;
+                } else {
+                    printf(", %c", hubs[i]);
+                }
+            }
+        }
+        if (tieFound) {
+            printf(" (also %d minutes)\n", maxTime);
+        }
     } else {
         printf("\nNo direct connections found.\n");
     }
